d6854bba2ab1ff736d67e9ecc3a59b6b02e32165
jaymeco <jayme.anunciacao@gmail.com>
2021-04-20 13:06:57 -0300

    Add create settings route

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,26 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { SettingRepository } from './repositories/SettingRepository';
 
 const routes = Router();
+
+routes.post('/settings', async (request, response)=>{
+  try {
+    const { chat, username } = request.body;
+
+    const settingRepository = getCustomRepository(SettingRepository);
+
+    const setting = await settingRepository.create({ chat, username });
+
+    await settingRepository.save(setting);
+
+    return response.status(201).json(setting);
+  } catch (error) {
+    return response.status(400).json({
+      message: error.message
+    })
+  }
+
+})
 
 export { routes };
